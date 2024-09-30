c3d1d0634f0d18d864268cf2ce70a7a22e31446b
TheP1geon <hauptmaverick@gmail.com>
2024-09-30 14:31:47 -0400

    Start setting up GLFW and OpenGL

--- sim/src/main.c
+++ sim/src/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+
 int main(void) {
-    printf("Hello, World!\n");
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    window = glfwCreateWindow(540, 540, "Simulation", 0, 0);
+    if (!window) {
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glClearColor(0.5, 0.f, 0.5, 1.f);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(0);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
