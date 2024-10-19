8255e0acc18ec43f38a820a5600aa1ea608d3898
TheP1geon <hauptmaverick@gmail.com>
2024-10-19 16:21:24 -0400

    Make cube Prepare to go 3D

--- sim/src/main.zig
+++ sim/src/main.zig
@@ -8,7 +8,35 @@ const VAO = object.VAO;
 const VBO = object.VBO;
 const EBO = object.EBO;
 
+const Camera = @import("Camera.zig");
+
 var procs: gl.ProcTable = undefined;
+
+var wireframe: bool = false;
+
+fn toggle_wireframe() void {
+    wireframe = !wireframe;
+}
+
+fn window_size_callback(window: glfw.Window, width: i32, height: i32) void {
+    _ = window;
+    gl.Viewport(0, 0, @intCast(width), @intCast(height));
+}
+
+fn key_callback(
+    window: glfw.Window,
+    key: glfw.Key,
+    scancode: i32,
+    action: glfw.Action,
+    mods: glfw.Mods,
+) void {
+    _ = .{ window, scancode, mods };
+    if (action == .press) {
+        if (key == .space) {
+            toggle_wireframe();
+        }
+    }
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -29,7 +57,7 @@ pub fn main() !void {
     defer glfw.terminate();
 
     // Create our window
-    const window_ = glfw.Window.create(640, 480, "Physics Simulation", null, null, .{
+    const window_ = glfw.Window.create(600, 600, "Physics Simulation", null, null, .{
         .resizable = false,
         .context_version_major = 3,
         .context_version_minor = 3,
@@ -42,6 +70,9 @@ pub fn main() !void {
     const window = window_.?;
     defer window.destroy();
 
+    window.setKeyCallback(key_callback);
+    window.setSizeCallback(window_size_callback);
+
     glfw.makeContextCurrent(window);
     defer glfw.makeContextCurrent(null);
 
@@ -51,16 +82,39 @@ pub fn main() !void {
     gl.makeProcTableCurrent(&procs);
     defer gl.makeProcTableCurrent(null);
 
-    gl.Viewport(0, 0, 640, 480);
+    gl.Viewport(
+        0,
+        0,
+        @intCast(window.getSize().width),
+        @intCast(window.getSize().height),
+    );
 
     const vertices = [_]f32{
-        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-        0.5,  -0.5, 0.0, 0.0, 1.0, 0.0,
-        0,    0.5,  0.0, 0.0, 0.0, 1.0,
+        // front
+        0.5, 0.5, 0.5, 1.0, 0.0, 1.0, // top right
+        -0.5, 0.5, 0.5, 1.0, 0.0, 1.0, // top left
+        0.5, -0.5, 0.5, 1.0, 0.0, 1.0, // bottom right
+        -0.5, -0.5, 0.5, 0.0, 1.0, 1.0, // bottom left
+        // back
+        0.5, 0.5, -0.5, 0.0, 1.0, 1.0, // top right
+        -0.5, 0.5, -0.5, 1.0, 1.0, 1.0, // top left
+        0.5, -0.5, -0.5, 0.0, 0.0, 1.0, // bottom right
+        -0.5, -0.5, -0.5, 1.0, 0.0, 1.0, // bottom left
     };
 
     const indices = [_]gl.uint{
         0, 1, 2,
+        3, 1, 2,
+        4, 6, 0,
+        2, 6, 0,
+        6, 7, 4,
+        5, 7, 4,
+        3, 1, 7,
+        5, 1, 7,
+        5, 4, 1,
+        0, 4, 1,
+        2, 3, 7,
+        2, 6, 7,
     };
 
     var vao = VAO.init();
@@ -92,13 +146,20 @@ pub fn main() !void {
     defer shader.deinit();
     shader.use();
 
+    gl.Enable(gl.DEPTH_TEST);
+
     gl.ClearColor(0.02, 0.2, 0.27, 1);
     while (!window.shouldClose()) {
-        gl.Clear(gl.COLOR_BUFFER_BIT);
+        gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        if (wireframe) {
+            gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE);
+        } else {
+            gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL);
+        }
 
         const time = glfw.getTime();
         shader.set_float("time", @floatCast(time));
-        shader.use();
 
         gl.DrawElements(gl.TRIANGLES, @intCast(ebo.indices.len), gl.UNSIGNED_INT, 0);
 
